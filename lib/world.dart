35d9bb64183ea70aef5ed8993b1d77cdce50b2c1
David Domkář <dave.domkar@gmail.com>
2020-02-28 08:31:26 +0100

    feat: add world class with addSystem method

---
+++ lib/world.dart
@@ -0,0 +1,22 @@
+import 'dart:collection';
+
+import 'package:dartex/component.dart';
+import 'package:dartex/system.dart';
+
+class World {
+  final List<System> _systems;
+  final HashMap<Type, HashMap<int, Component>> _componentMap;
+
+  World({List<System> systems})
+      : _systems = systems ?? [],
+        _componentMap = HashMap();
+
+  addSystem(System system) {
+    if (_systems.contains(system)) {
+      print(
+          "[DARTEX]: Warning: System is already added and will be updates more than once!");
+    }
+
+    _systems.add(system);
+  }
+}
